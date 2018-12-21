630214fae305716fc8c6d715c34ce41642dd6e8f
computerfairy <35657610+computerfairy@users.noreply.github.com>
2018-12-21 17:54:36 +0900

    Add files via upload
    TODO를 메인 메뉴에 합쳤습니다

---
+++ TodoList.java
@@ -0,0 +1,34 @@
+public class TodoList {
+	
+	private String createdate;
+	private String Due;
+	private String Description;
+
+
+	public String getCreatedate() {
+		return createdate;
+	}
+
+	public void setCreatedate(String createdate) {
+		this.createdate = createdate;
+	}
+
+	public String getDue() {
+		return Due;
+	}
+
+	public void setDue(String due) {
+		Due = due;
+	}
+
+	public String getDescription() {
+		return Description;
+	}
+
+	public void setDescription(String description) {
+		Description = description;
+	}
+	
+	
+
+}
