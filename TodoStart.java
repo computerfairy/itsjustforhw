d220a370a53ce4bbb51904ea2365400632259b27
computerfairy <35657610+computerfairy@users.noreply.github.com>
2018-12-21 16:52:23 +0900

    Add files via upload
    Todo의 실행파일들과 Test 파일입니다!

---
+++ TodoStart.java
@@ -0,0 +1,16 @@
+public class TodoStart {
+	
+	public void start() {
+		Todo biz = new Todo();
+		
+		biz.startProgram();	
+	}
+
+	public static void main(String[] args) {
+		
+		TodoStart todostart = new TodoStart();
+		todostart.start();
+
+	}
+
+}
